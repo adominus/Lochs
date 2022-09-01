91e1d2815372465ed09b5987472c3b76b00bd078
Adominus <1979748+adominus@users.noreply.github.com>
2022-09-01 17:34:52 +0100

    5.4 A not very pretty printer

--- Lochs/Token.cs
+++ Lochs/Token.cs
@@ -4,16 +4,18 @@ public class Token
 {
     private readonly TokenType _tokenType;
     private readonly string _lexeme;
-    private readonly object _literal;
+    private readonly object? _literal;
     private readonly int _line;
 
-    public Token(TokenType tokenType, string lexeme, object literal, int line)
+    public Token(TokenType tokenType, string lexeme, object? literal, int line)
     {
         _tokenType = tokenType;
         _lexeme = lexeme;
         _literal = literal;
         _line = line;
     }
+
+    public string Lexeme => _lexeme;
 
     public override string ToString()
         => $"{_tokenType} {_lexeme} {_literal}";
