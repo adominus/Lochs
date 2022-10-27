f2bd0fa0805bcf99785f8943a472098d2f2c170d
Adominus <1979748+adominus@users.noreply.github.com>
2022-10-27 07:27:04 +0100

    Interpret binary and throw runtime exceptions

---
+++ Lochs/RuntimeException.cs
@@ -0,0 +1,13 @@
+﻿namespace Lochs
+{
+    public class RuntimeException : Exception
+    {
+        public Token Token { get; }
+
+        public RuntimeException(Token token, string message)
+            : base(message)
+        {
+            Token = token;
+        }
+    }
+}
